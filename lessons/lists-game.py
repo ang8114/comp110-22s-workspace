75892f9f2e1058fc5dc4f8d8f36d59bee4dac226
angela lian <alian@unc.edu>
2022-03-23 22:29:13 -0400

    final

--- lessons/lists-game.py
+++ lessons/lists-game.py
@@ -1,9 +1,17 @@
 """Examples of using lists in a simple 'game'."""
 
 
-from random import randint
+def vowels(word: str) -> str:
+    """Return."""
+    i: int = 0
+    vowels: list[str] = ["a", "e", "i", "o", "u"]
+    final: str = ""
+    while i < len(word):    
+        for vowels[i] in word:
+            final += vowels[i]
+            i += 1
+        if word[i] % 3 == 0:
+            final += word[i]
+            i += 1
 
-
-rolls: list[int] = list()
-rolls.append(1)
-print(rolls)
+    print(final)
